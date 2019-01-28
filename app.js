009adad0359ebbc92fa97cdf5b64ee5fd2c51a57
Ashish Patel <ashishpatel@skaas.guru>
2019-01-28 11:24:54 +0530

    generate pdfs using puppeteer

--- app.js
+++ app.js
@@ -1,19 +1,38 @@
 const puppeteer = require('puppeteer');
 
-const takeScreenshot = async () => {
+const generatePDF = async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     const options = {
-        path: 'images/screen.png',
-        fullPage: true,
-        omitBackground: true
-    };
+        waitUntil: 'networkidle2'
+    }
 
-    await page.goto('http://ashishpatel.ml');
-    await page.waitForSelector('.social-me');
-    await page.screenshot(options);
+    await page.goto('http://ashishpatel.ml', options);
+    await page.pdf({
+        path: 'pdfs/website.pdf',
+        format: 'A4'
+    })
 
     await browser.close();
 }
 
-takeScreenshot();
+generatePDF();
+
+
+// const takeScreenshot = async () => {
+//     const browser = await puppeteer.launch();
+//     const page = await browser.newPage();
+//     const options = {
+//         path: 'images/screen.png',
+//         fullPage: true,
+//         omitBackground: true
+//     };
+
+//     await page.goto('http://ashishpatel.ml');
+//     await page.waitForSelector('.social-me');
+//     await page.screenshot(options);
+
+//     await browser.close();
+// }
+
+// takeScreenshot();
