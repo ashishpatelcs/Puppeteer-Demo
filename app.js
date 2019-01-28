fed52e88b6639a461e78381755ef5be55b609219
Ashish Patel <ashishpatel@skaas.guru>
2019-01-28 12:20:55 +0530

    completed pdf generation from html content sent in request body

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const puppeteer = require('puppeteer');
+const fs = require('fs');
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
@@ -14,14 +15,43 @@ app.get('/', (req, res) => {
 })
 
 app.post('/', (req, res) => {
-    let body = req.body;
-    let html = body.html;
-    res.writeHead(200, {
-        'Content-Type': 'text/html',
-        'Content-Length': html.length,
-        'Expires': new Date().toUTCString()
+    let html = req.body.html;
+    // let html = body.html;
+
+    fs.writeFile('files/my.html', html, (err) => {
+        if(err) {
+            return console.log(err);
+        } else {
+            (async () => {
+                const browser = await puppeteer.launch();
+                const page = await browser.newPage();
+                const options = {
+                    waitUntil: 'networkidle2'
+                }
+    
+                await page.goto('http://localhost:3000/html/my.html', options);
+                await page.pdf({
+                    path: 'pdfs/website.pdf',
+                    format: 'A4'
+                })
+    
+                await browser.close();
+
+                res.writeHead(200, {
+                    'Content-Type': 'application/pdf',
+                    'Expires': new Date().toUTCString()
+                });
+                res.end('http://localhost:3000/download/website.pdf');
+            })();
+        }
     });
-    res.end(html);
+
+    // res.writeHead(200, {
+    //     'Content-Type': 'text/html',
+    //     'Content-Length': html.length,
+    //     'Expires': new Date().toUTCString()
+    // });
+    // res.end(html);
 })
 
 app.listen(PORT, () => {
