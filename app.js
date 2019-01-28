44fbb70acc434e08ed977785cfad0cce62ba865d
Ashish Patel <ashishpatel@skaas.guru>
2019-01-28 11:43:25 +0530

    adding feature to send html content to the api which creates a pdf from it and returns

--- app.js
+++ app.js
@@ -1,4 +1,27 @@
 const puppeteer = require('puppeteer');
+const express = require('express');
+const app = express();
+
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+    res.send('API Works');
+})
+
+app.post('/', (req, res) => {
+    let body = req.body;
+    let html = body.html;
+    res.writeHead(200, {
+        'Content-Type': 'text/html',
+        'Content-Length': html.length,
+        'Expires': new Date().toUTCString()
+    });
+    res.end(html);
+})
+
+app.listen(PORT, () => {
+    console.log('Server running on http://localhost:'+PORT);
+})
 
 const generatePDF = async () => {
     const browser = await puppeteer.launch();
