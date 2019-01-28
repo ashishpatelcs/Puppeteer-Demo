ab7b0b66e55346f422b4801a22eb559a00e485cd
Ashish Patel <ashishpatel@skaas.guru>
2019-01-28 11:07:05 +0530

    take screenshot using puppeteer in node.js

---
+++ app.js
@@ -0,0 +1,18 @@
+const puppeteer = requrie('puppeteer');
+
+const takeScreenshot = async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    const options = {
+        path: 'images/screen.png',
+        fullPage: true,
+        omitBackground: true
+    };
+
+    await page.goto('http://ashishpatel.ml');
+    await page.screenshot(options);
+
+    await browser.close();
+}
+
+takeScreenshot();
